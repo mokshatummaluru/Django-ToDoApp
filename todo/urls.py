a7d6f6f2399498d94dcbf8417ba7b2dfe9123f9f
MokshaTummaluru <112241855+mokshatummaluru@users.noreply.github.com>
2026-02-23 14:18:24 +0530

    Developed all CRUD opeartions and functionable buttons

--- todo/urls.py
+++ todo/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('addTask/',views.addTask,name='addTask'),
+    path('mark_as_done/<int:pk>/',views.mark_as_done,name='mark_as_done'),
+    path('edit_task/<int:pk>/',views.edit_task,name='edit_task'),
     
-    
-    
-    
+    path('delete_task/<int:pk>/',views.delete_task,name='delete_task'),
 ]
